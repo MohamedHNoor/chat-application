2e1352b2f5bc360f2bb35efa2f2681e94538c399
Mohamed Hassan Noor <mohmed.hassannoor114@gmail.com>
2024-01-21 11:48:23 +0200

    Created Channel container for users to communicate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
@@ -6,6 +6,7 @@ import Cookies from 'universal-cookie';
 import { ChannelContainer, ChannelListContainer, Auth } from './components';
 
 // styles
+import 'stream-chat-react/dist/css/index.css';
 import './App.css';
 
 const cookies = new Cookies();
@@ -30,12 +31,27 @@ if (authToken) {
 }
 
 const App = () => {
+  const [createType, setCreateType] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   if (!authToken) return <Auth />;
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme='team light'>
-        <ChannelListContainer />
-        <ChannelContainer />
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
+        />
       </Chat>
     </div>
   );
